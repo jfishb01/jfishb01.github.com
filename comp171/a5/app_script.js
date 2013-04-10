1647d6cda63d3e0a6922844acbe161839faba17c
krys <krys.danielewicz@gmail.com>
2013-04-09 20:24:12 -0400

    merging changes

--- comp171/a5/app_script.js
+++ comp171/a5/app_script.js
@@ -41,9 +41,39 @@ Ingredients[38] = "1/4 lb Basil";
 Ingredients[39] = "1/2 lb Cheddar Cheese";
 Ingredients[40] = "4 Chilli Pepper";
 
+Recipes = new Array();
+Recipes[0] = "Chicken Burritos";
+Recipes[1] = "Beef Stew";
+Recipes[2] = "Vegetarian Chili";
+Recipes[3] = "Chicken Picatta";
+Recipes[4] = "Eggplant Parmigiana";
+Recipes[5] = "Ratatouille";
+Recipes[6] = "Corn Chowder";
+Recipes[7] = "Creamy Mushroom Meatloaf";
+Recipes[8] = "Grilled Mongolian Pork Chops";
+Recipes[9] = "Chicken Enchilada Casserole";
+Recipes[10] = "Jamabalaya";
+Recipes[11] = "Nachos Supreme";
+Recipes[12] = "Bruschetta Chicken Skillet";
+Recipes[13] = "Skillet Salmon";
+Recipes[14] = "Tunisian Vegetable Couscous";
+
 Instructions = new Array();
 Instructions["Chicken Burritos"] = "--- Directions ---\n1. Preheat oven to 350 degrees\n2. Sautee chicken on stove with\nvegetable oil\n3. Sautee mushrooms and zuchini on\nstove with vegetable oil\n4. Sautee black beans on stove\n5. Mix chicken, mushrooms, zuchini,\nsalsa, and black beans in a bowl\n6. Wrap mixed ingredients in\n7. Sprinkle cheese on top of the\ntortillas\n8. Cook in oven for 25 minutes\n9. Let sit for 5 minutes to cool";
-Instructions["poop"] = "hey";
+Instructions["Beef Stew"] = "---Directions---\n1.	On medium-high heat, add the \nvegetable oil to a large heavy\npot (one that has a \ntight fitting lid).\n2. When it begins to smoke slightly, \nadd the beef and brown very well. \nDo in batches if necessary. \nAdd the salt and pepper as the \nbeef browns.\n3. Once browned, remove the beef with \na slotted spoon set aside.\n4. Add the onions and sauté for \nabout 5 minutes, until softened.\n5. Reduce heat to medium-low, and \nadd the flour and cook for 2 \nminutes stirring often.\n6. Add the garlic and cook \nfor 1 minute.\n7. Add wine and deglaze the pan, \nscraping any brown bits stuck \nto the bottom of the pan. The \nflour will start to thicken \nthe wine as it comes to a simmer.\n8. Simmer wine for 5 minutes, \nand then add the broth, bay leaves, \nthyme, rosemary, and the beef.\n9.	Bring back to a gentle simmer, cover \nand cook on very low for \nabout 1 hour.\n10. Add potatoes, carrots, and celery, and simmer covered \nfor another 30 minutes or until the \nmeat and vegetables are tender. \nTaste and adjust seasoning.\n11. Turn off heat and let sit for \n15 minutes before serving. Garnish \nwith the fresh parsley if desired.";
+Instructions["Vegetarian Chili"] = "---Directions---\n1. In a large pot, heat oil \nover medium-high.\n2. Add onion and garlic; cook, \nstirring frequently, until \nonion is translucent and \ngarlic is soft, about 4 \nminutes.\n3. Add cumin and chile powder, season \nwith salt and pepper, and cook \nuntil spices are fragrant, \n1 minute.\n4. Add zucchini and tomato \npaste; cook, stirring frequently, \nuntil tomato paste is deep brick \nred, 3 minutes.\n5. Stir in black beans, \npinto beans, and both cans \ndiced tomatoes. Add 2 cups water \nand bring mixture to a boil.\n6. Reduce to a medium simmer and \ncook until zucchini is tender \nand liquid reduces slightly, \n20 minutes.\n7.Season with salt and \npepper.";
+Instructions["Chicken Picatta"] = "---Directions---\nSeason chicken with salt and pepper.\n2. Dredge chicken in flour \nand shake off excess.\n3. In a large skillet over \nmedium high heat, melt 2 \ntablespoons of butter with 3 \ntablespoons olive oil.\n4. When butter and oil start \nto sizzle, add 2 pieces of \nchicken and cook for 3 minutes.\n5. When chicken is browned, \nflip and cook other side \nfor 3 minutes.\n6. Remove and transfer to plate.\n7. Melt 2 more tablespoons butter \nand add another 2 tablespoons \nolive oil.\n8. When butter and oil start \nto sizzle, add the other 2 \npieces of chicken and \nbrown both sides in same \nmanner.\n9. Remove pan from heat and \nadd chicken to the plate.\n10. Into the pan add the lemon juice, \nstock and capers. \n11. Return to stove and bring to \nboil, scraping up brown \nbits from the pan for extra \nflavor. \n12. Check for seasoning.\n13. Return all the chicken to the \npan and simmer for 5 minutes.\n14. Remove chicken to platter.\n15. Add remaining 2 tablespoons butter \nto sauce and whisk vigorously.\n16. Pour sauce over chicken \nand garnish with parsley.";
+Instructions["Eggplant Parmigiana"] = "---Directions---\n1. Preheat the oven to 450 degrees F.\n2. Using some extra-virgin olive oil, oil \na baking sheet.\n3. Slice each eggplant into 6 pieces about \n1 to 1 1/2 inches thick. \n4. Lightly season each disk with salt and \npepper and place on the oiled sheet.\n5. Bake the eggplant at 450 degrees F \nuntil the slices begin turning deep brown \non top, about 12-15 minutes. \n6. Remove the eggplants from the oven.\n7. Remove the slices from the baking \nsheet and place them on a plate to cool.\n8. Lower oven temperature to 350 degrees F. \n9. In an 8 by 12-inch brownie pan, place the \n4 largest eggplant slice evenly spaced apart. \n10. Over each slice, spread 1/4 cup of tomato \nsauce and sprinkle with a teaspoon of basil.\n11. Place one slice of mozzarella over each \nand sprinkle with 1 teaspoon grated Parmigiano.\n12. Place the smaller slices of eggplant over \neach of the disks and repeat with tomato sauce, \nbasil, and the 2 cheeses. \n13. Repeat the layering again until all the \ningredients are used.\n14. Sprinkle the toasted bread crumbs over the \nof the eggplant dish, and bake uncovered \nuntil the cheese is melts and the tops turn light \nbrown, about 20 minutes. \n15. Serve immediately.";
+Instructions["Ratatouille"] = "---Directions---\n1.	Preheat oven to 350 degrees F \n(175 degrees C). Coat bottom\nand sides of a 1 1/2 quart \ncasserole dish with 1 tablespoon \nolive oil.\n2.	Heat remaining 1 tablespoon \nolive oil in a medium skillet over \nmedium heat. Cook and stir garlic \nuntil lightly browned. Mix in parsley \nand eggplant. Cook and stir until \neggplant is soft, about 10 minutes. \nSeason with salt to taste.\n3.	Spread eggplant mixture evenly \nacross bottom of prepared casserole \ndish. Sprinkle with a few tablespoons \nof Parmesan cheese. Spread zucchini \nin an even layer over top. Lightly salt \nand sprinkle with a little more cheese. \nContinue layering in this fashion, with \nonion, mushrooms, bell pepper, and \ntomatoes, covering each layer with a \nsprinkling of salt and cheese.\n4.	Bake in preheated oven for 45 minutes.";
+Instructions["Corn Chowder"] = "---Directions---\n1.	In large saucepan, melt margarine over\nmedium high heat. Add celery and \nonion; cook and stir until tender, \nabout 5 minutes.\n2.	Add chicken broth; bring to a boil. \nAdd potatoes; cook over low heat \nfor 20 to 25 minutes or until potatoes \nare soft, stirring occasionally.\n3.	Stir in corn and chiles; return to boiling. \nDissolve gravy mix in milk; stir into \nboiling mixture. Add cheese; cook and \nstir over low heat until cheese is melted.";
+Instructions["Creamy Mushroom Meatloaf"] = "---Directions---\n1.	Preheat the oven to 325 degrees F \n(165 degrees C).\n2.	Melt butter in an oven-safe skillet \nover medium-high heat. Stir in \nmushrooms and a pinch of salt; cook \nand stir until mushrooms begin to \nbrown, about 5 minutes.\n3.	Stir in fresh rosemary. Add flour \nand stir to coat the mushrooms; \ncook and stir for about 3 minutes.\n4.	Whisk in beef broth, 1/2 cup at a \ntime, whisking constantly to \nprevent lumps.\n5.	Turn heat to high and bring \nthe sauce to a simmer. Simmer \na few minutes until sauce starts \nto thicken. Season with salt and \npepper to taste.\n6.	Remove from heat and stir in \nheavy cream.\n7.	Slide prepared meatloaf into \nthe sauce. Spoon sauce over \nthe top of the meatloaf.\n8.	Bake in the preheated oven until \nno longer pink in the center, about \n1 1/2 hours. An instant-read thermometer \ninserted into the center should read \nat least 160 degrees F (70 degrees C).\n9.	Remove pan from the oven and gently \nremove meatloaf to a serving platter.\n10.	Skim off any extra fat from the \nsurface of the sauce.\n11.	Bring the sauce to a boil over\n medium-high heat to reduce until thick, \nabout 5 minutes.";
+Instructions["Grilled Mongolian Pork Chops"] = "---Directions---\n1.	Combine hoisin sauce, garlic, soy sauce, \nginger, 1 tablespoon red wine vinegar, \nrice vinegar, sherry vinegar, sesame oil, \n2 teaspoons sugar, hot sauce, white pepper, \nand black pepper in a large bowl. Whisk \nthoroughly and set aside.\n2.	Place pork chops in a resealable freezer\n bag; pour just more than 1/2 of marinade \ninto freezer bag over pork chops. Seal \nbag and refrigerate for 6 to 8 hours. \nReserve remaining marinade.\n3.	Combine 1/4 cup red wine vinegar, \n3 tablespoons sugar, 2 tablespoons \nhot mustard powder, and egg yolk in \na small saucepan over medium-low heat. \nWhisk until slightly thickened, about 5 \nminutes; remove from heat.\n4.	Stir in creme fraiche, Dijon mustard, \nturmeric, and cayenne pepper. \nRefrigerate until needed.\n5.	Remove pork chops from marinade \nand pat dry using paper towel.\n6.	Preheat an outdoor grill for high heat, \nand lightly oil the grate.\n7.	Cook pork chops on the preheated grill \nuntil browned grill marks appear, about \n4 minutes per side.\n8.	Move pork chops from directly above heat \nsource. Continue cooking over indirect medium \nheat, brushing the remaining marinade on each \nside, until no longer pink inside, about 25 minutes. \nAn instant-read thermometer inserted into the \ncenter should read 145 degrees F (63 degrees C).";
+Instructions["Chicken Enchilada Casserole"] = "---Directions---\n1.	Preheat the oven to 375 degrees F \n(190 degrees C). Grease a 9x9-inch \nbaking dish.\n2.	Mix tomato sauce, water, taco seasoning \nmix, and chili powder together in a \nsaucepan; bring to a simmer over medium\n heat.\n3.	Heat vegetable oil in a skillet over medium \nheat and brown chicken tenderloins on \nboth sides, about 5 minutes per side. \nPour tomato sauce mixture over the \nchicken, bring to a simmer, and cook over \nmedium-low heat until chicken tenderloins are\n no longer pink inside, about 8 minutes. \nTransfer chicken to a bowl and shred; \nreturn shredded chicken to the sauce. Mix \nin black beans and cream cheese until \nthoroughly combined.\n4.	Pour chicken mixture into prepared baking \ndish. Top with shredded Mexican cheese. \nWhisk corn bread mix, egg, and milk in a \nbowl, and spoon the batter over the chicken mix.\n5.	Bake in the preheated oven until the casserole is \nbubbling and the corn bread topping is \nbrowned and set, about 30 minutes.";
+Instructions["Jambalaya"] = "---Directions---\n1.	In a bowl, toss Johnsonville Andouille \nDinner Sausage, shrimp OR chicken and \nCreole seasoning. In a skillet, cook the \nsausage mixture in oil over medium \nheat until shrimp or chicken is fully \ncooked; set aside.\n2.	Prepare jambalaya rice mix according to \npackage directions, adding half of the \ntomatoes.\n3.	About ten minutes before the rice is \ndone and while it is still firm, add the \nremaining tomatoes, Worcestershire \nsauce, hot sauce and parsley. Partially \ncover and finish cooking. Stir in sausage \nmixture.\n4.	To serve, taste and adjust seasonings, \nadding a little additional Creole seasoning \nif you want it more spicy. Spoon into large \nbowls.";
+Instructions["Nachos Supreme"] = "---Directions---\n1.	Cook beef and taco seasoning in \nskillet until browned. Pour off fat.\n2.	Add soup, water and rice. Heat to \na boil. Cover and cook over low \nheat 5 min. or until done.\n3.	Top with salsa, cheese and lettuce. \nServe with tortilla chips for dipping.";
+Instructions["Bruschetta Chicken Skillet"] = "---Directions---\n1.	Heat oil in large skillet on medium heat. \nAdd chicken; cook 3 to 4 min. on each \nside or until done (165 degrees F). Meanwhile, \ncombine tomatoes, mozzarella, 2 Tbsp. \nParmesan and basil.\n2.	Add reserved tomato liquid to skillet. Top chick\n3.	en with tomato mixture; simmer on low \nheat 5 min. or until mozzarella is melted \nand tomato mixture is heated through.\n4.	Transfer chicken to serving plate; top \nwith crushed croutons and remaining Parmesan";
+Instructions["Skillet Salmon"] = "---Directions---\n1.	Heat large skillet sprayed with cooking spray \non medium-high heat. Add fish; cook 5 min. \non each side or until fish flakes easily with fork. \nRemove from skillet; cover to keep warm.\n2.	Add milk and reduced-fat cream cheese to skillet; \ncook and stir until cream cheese is completely \nmelted and mixture is well blended. Stir in \ncucumbers and dill.\n3.	Return fish to skillet. Cook 2 min. or until \nheated through. Serve over rice; top with \ncream cheese sauce.\n";
+Instructions["Tunisian Vegetable Couscous"] = "---Directions---\n1.	Heat oil in large pot over medium-low heat. \nPlace onion, zucchini, yellow squash, and \ncarrot in pot and cook, stirring occasionally, \nuntil onions begin to soften and turn translucent, \nabout 5 minutes. Stir in the red and yellow peppers \nand mushrooms; cook another 3 minutes. \nVegetables should still be firm.\n2.	Pour in the vegetable stock and season with \npaprika, cardamom, salt, and cilantro. Bring \nmixture to a boil; reduce heat to low. Stir in \nchickpeas and tomatoes. Slowly pour in the \ncouscous; stir. Cover pot immediately and \nremove from the heat. Let stand, covered, for \n5 minutes. Fluff couscous with a fork. The couscous \nshould have absorbed about half the cooking liquid.\n3.	Serve at slightly warmer than room temperature. \nGarnish with orange zest and the grated cheese, \npaprika, and almonds.";
 
 window_stack = new Array();
 window_stack.push("index.html");
@@ -58,12 +88,36 @@ function openWindow(file)
 function generate()
 {
   str = "";
-  for(i = 0; i < 18; i++)
+  for(var i = 0; i < 18; i++)
   {  
     str += Ingredients[Math.floor(Math.random()*41)] + "\n";
   }
   var txtArea = document.getElementById('groceries');
   txtArea.value = str;
+  
+  slct = document.getElementById('recipes');
+  var used = new Array();
+  for(var i = 0; i < 7; i++)
+  {
+    var newOption = document.createElement("option");
+    var index = 0;
+    var indexUsed = true;
+    while(indexUsed)
+    {
+      indexUsed = false;
+      index = Math.floor(Math.random()*15)
+      for(var j in used)
+      {
+        if(index == used[j])
+        {
+          indexUsed = true;
+        }
+      }
+    }
+    newOption.innerHTML = Recipes[index];
+    slct.add(newOption);
+    used.push(index);
+  }
 }
 
 function instructions(file)
@@ -75,12 +129,13 @@ function instructions(file)
   $("#body_content").trigger("create");
   $("#back_link").attr("href", "javascript:location.reload();");
   $("#back_link").removeAttr("data-rel");
-  setTextArea(str, file);
+  setTextArea(str);
 }
 
-function setTextArea(str, file)
+function setTextArea(str)
 {
   var txtArea = document.getElementById('instructions');
+  console.log(Instructions[str]);
   txtArea.value = Instructions[str];
 }
 
